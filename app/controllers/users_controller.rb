cc03d22a8a4dc099ca5607cc7732601755c6666f
Matthew Rivera <matthewjesseissac@gmail.com>
2018-09-09 20:36:39 -0400

    Delete and back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   # before_action :find_user, only: %i[show edit update destroy]
 
   def index
-    # @user = User.new
+    # @user = User.all.order("created_at DESC")
+    # @user = User.all.order("created_at ASC")
     @user = User.all
   end
 
@@ -17,6 +18,7 @@ end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
     redirect_to '/users'
     #root_path
@@ -32,17 +34,15 @@ end
     # @user = User.new(user_params)
     if @user.update_attributes(user_params)
     redirect_to '/users'
+  else
+    render 'edit'
   end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    if @user = User.destroy(user_params)
-      redirect_to users_path
-  else
-render ('new')
-  end
-  end
+  @user = User.destroy(params[:id])
+  redirect_to '/users'
+end
 
   private
   def user_params
